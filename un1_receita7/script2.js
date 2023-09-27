edf75a0c5fde745cab4de878b3e97be439b3a84a
Victor Ryan <vitorsilva3612@gmail.com>
2023-09-27 14:54:31 -0300

    Receita 7 passo 6

--- un1_receita7/script2.js
+++ un1_receita7/script2.js
@@ -1,14 +1,18 @@
 let cervejas = []
-      //cs é um array de cervejas
-      const carregarDiv = cs => {
-         const div = document.getElementById("cervejasDiv")
-         const itensHtml = cs.map(({ name, alcohol }) => `<div>${name} -- ${alcohol}</div>`)
-         div.innerHTML = `${itensHtml.join("\n")}`
-      }
-      async function carregarCervejas(){
-         let res = await fetch("https://random-data-api.com/api/v2/beers?size=3")
-         cervejas = await res.json()
-         carregarDiv(cervejas)
-      }
-      let botao = document.getElementById("botaoCarregar")
-      botao.addEventListener("click", () => carregarCervejas())
+//cs é um array de cervejas
+const carregarDiv = cs => {
+   const div = document.getElementById("cervejasDiv")
+   const itensHtml = cs.map(({ name, alcohol }) => `<div>${name} -- ${alcohol}</div>`)
+   div.innerHTML = `${itensHtml.join("\n")}`
+}
+async function carregarCervejas(){
+   try{
+      let res = await fetch("https://random-data-api.com/api/v2/beers?size=3")
+      cervejas = await res.json()
+      carregarDiv(cervejas)
+   }catch(err){
+      document.getElementById("cervejasDiv").innerHTML = "Aconteceu alguma problema..."
+   }
+}
+let botao = document.getElementById("botaoCarregar")
+botao.addEventListener("click", () => carregarCervejas())

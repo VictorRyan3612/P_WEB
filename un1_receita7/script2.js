dc6183303365a4a62c2bac0fa3dc1fc1eebcce13
Victor Ryan <vitorsilva3612@gmail.com>
2023-10-04 14:01:14 -0300

    Adaptação de R6 para R7 parte 2

--- un1_receita7/script2.js
+++ un1_receita7/script2.js
@@ -1,10 +1,22 @@
+let isNomeExibido = false
 let cervejas = []
 //cs é um array de cervejas
-const carregarDiv = (cervejasArray, id="cervejasDiv") => {
+const carregarDiv = (cervejasArray, id="cervejasDiv", cabecalhos = ["Nome", "Álcool", "Estilo", "Amargor"]) => {
    const div = document.getElementById(id)
-   const itensHtml = cervejasArray.map(({ name, alcohol }) => `<div>${name} -- ${alcohol}</div>`)
-   div.innerHTML = `${itensHtml.join("\n")}`
+   if (isNomeExibido) {
+      div.style.opacity = 0;
+      setTimeout(() => {
+         div.innerHTML = '';
+      }, 500);
+   } else {
+      setTimeout(() => {
+         const itensHtml = cervejasArray.map(({ name, alcohol }) => `<div>${name} -- ${alcohol}</div>`)
+         div.innerHTML = `${itensHtml.join("\n")}`
+      }, 1);
+   };
+   isNomeExibido = !isNomeExibido;
 }
+
 async function carregarCervejas(){
    try{
       let res = await fetch("https://random-data-api.com/api/v2/beers?size=3")

69d89436897d9c8f2c7d7a7e93e576caa29fc4e9
Victor Ryan <vitorsilva3612@gmail.com>
2023-11-09 17:10:59 -0300

    Receita 15
    Next

---
+++ un2_receita5/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default function App(){
+   return (
+      <div>
+         <h2>
+            Receita 15
+         </h2>
+
+         <p>
+            
+         </p>
+      </div>
+   )
+}

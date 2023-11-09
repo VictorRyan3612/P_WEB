e95c57dce94ac570dee048c9d8e940f65f38365a
Victor Ryan <vitorsilva3612@gmail.com>
2023-11-09 18:54:47 -0300

    Copia de R15
    Cópia de R15 do commit disponível nesse link: https://github.com/VictorRyan3612/P_WEB/commit/545076f0d84f9d71a4392b3c452e2ec0c9bb0dec

---
+++ un2_receita6/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function App(){
+   return (
+      <div>
+         <center>
+         <h1>
+            Receita 16
+         </h1>
+
+         </center>
+      </div>
+   )
+}

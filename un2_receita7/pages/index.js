6ace50fb9528a591f05ae94d20a611d29df57749
Victor Ryan <vitorsilva3612@gmail.com>
2023-11-10 09:22:55 -0300

    Copia de R15 e links

--- un2_receita7/pages/index.js
+++ un2_receita7/pages/index.js
@@ -5,9 +5,30 @@ export default function App(){
    return (
       <div>
          <center>
-         <h1>
-            Receita 17
-         </h1>
+            <h1>
+               Receita 17
+            </h1>
+            <a href="https://p-web-seven.vercel.app/movies1" target="_blank"> 
+               Movies 1
+            </a>
+            <br></br>
+         
+            <a href="https://p-web-seven.vercel.app/movies2" target="_blank"> 
+               Movies 2
+            </a>
+            <br></br>
+            
+         
+            <a href="https://p-web-seven.vercel.app/movies3" target="_blank"> 
+               Movies 3
+            </a>
+            <br></br>
+         
+            <a href="https://p-web-seven.vercel.app/moviesR15" target="_blank"> 
+               Movies Receita 15
+            </a>
+            <br></br>
+            
          </center>
       </div>
    )

7fc456a9d51cb8478e8e4a4940ff3eb5ace3a9c1
Victor Ryan <vitorsilva3612@gmail.com>
2023-11-29 12:46:23 -0300

    Receita 2 parte 2

---
+++ un2_next/pages/receita2/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Titulo({tituloContent}){
+   return (
+      <div>
+         <h2>
+            {tituloContent}
+         </h2>
+      </div>
+   )
+}
---
+++ un2_next/pages/receita2/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Titulo from './home';
+
+export default function Receita2(){
+   return (
+      <div>
+         <Titulo tituloContent='Titulo Principal'/>
+      </div>
+   )
+}

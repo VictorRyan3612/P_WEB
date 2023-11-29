0ce2ff23d670dc474d99801d99e7870023673c11
Victor Ryan <vitorsilva3612@gmail.com>
2023-11-29 11:01:33 -0300

    Receita 2 next

---
+++ unidade2_next/pages/receita2/novaPagina.js
@@ -0,0 +1,11 @@
+import {Titulo, Corpo} from './home';
+
+export default function SubPage2(){
+   return (
+      <div>
+         <Titulo tituloContent='Titulo de novaPagina'/>
+         <br></br>
+         <Corpo corpoContent='Conteudo de novaPagina'/>
+      </div>
+   )
+}

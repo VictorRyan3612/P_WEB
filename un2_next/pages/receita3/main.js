fe08ad3eed0c7d3e6d39b625b5ffbca223f1381a
Victor Ryan <vitorsilva3612@gmail.com>
2023-11-29 23:08:51 -0300

    Receita 3 next

---
+++ un2_next/pages/receita3/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TelaMovies from './movies';
+
+
+export default function App(){
+   return (
+      <div>
+         <center>
+         <h1>
+            Receita 3
+         </h1>
+         <h2>
+            Pesquisa de Filmes
+         </h2>
+         </center>
+         
+         <TelaMovies />
+         
+      </div>
+   )
+}

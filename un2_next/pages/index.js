6126e99d650cd77ac4735b68af76bc633317de52
Victor Ryan <vitorsilva3612@gmail.com>
2023-11-29 23:34:20 -0300

    Menu all receitas

--- un2_next/pages/index.js
+++ un2_next/pages/index.js
@@ -8,30 +8,30 @@ export default function App(){
             <h1>
                Receitas Next 2ª unidade
             </h1>
-            <a href="" target="_blank"> 
+            <a href="/receita1/main" target="_blank"> 
                Receita 1
             </a>
             <br></br>
          
-            <a href="" target="_blank"> 
+            <a href="/receita2/main" target="_blank"> 
                Receita 2
             </a>
             <br></br>
             
          
-            <a href="" target="_blank"> 
+            <a href="/receita3/main" target="_blank"> 
                Receita 3
             </a>
             <br></br>
 
 
-            <a href="" target="_blank"> 
+            <a href="/receita4/main" target="_blank"> 
                Receita 4
             </a>
             <br></br>
 
 
-            <a href="" target="_blank"> 
+            <a href="/receita5/main" target="_blank"> 
                Receita 5
             </a>
             <br></br>

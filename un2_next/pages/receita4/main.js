75b73b24c2c0247bee7430f4368eaa5f8e4e9723
Victor Ryan <vitorsilva3612@gmail.com>
2023-11-29 23:21:18 -0300

    Receita 4 next

---
+++ un2_next/pages/receita4/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function App(){
+   return (
+      <div>
+         <center>
+         <h1>
+            Receita 4
+         </h1>
+         </center>
+      </div>
+   )
+}

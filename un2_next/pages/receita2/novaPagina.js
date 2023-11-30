1554cd76041c35161b6648e82b01969f0a1f45f2
Victor Ryan <vitorsilva3612@gmail.com>
2023-11-29 22:58:14 -0300

    Receita 2 parte 4

---
+++ un2_next/pages/receita2/novaPagina.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Titulo from './home';
+import Corpo from './home';
+
+export default function SubPage2(){
+   return (
+      <div>
+         <Titulo tituloContent='Titulo de novaPagina'/>
+         <br></br>
+         <Corpo corpoContent='Conteudo de novaPagina'/>
+      </div>
+   )
+}

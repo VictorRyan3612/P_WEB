7a8616930bdc0e561e8ac73d8a24b8f0fc4104ab
Victor Ryan <vitorsilva3612@gmail.com>
2023-12-04 00:04:09 -0300

    Bug Fix

--- un2_next/pages/receita5/main.js
+++ un2_next/pages/receita5/main.js
@@ -8,23 +8,23 @@ export default function App(){
             <h1>
                Receita 17
             </h1>
-            <a href="/movies1" target="_blank"> 
+            <a href="/receita5/movies" target="_blank"> 
                Movies 1
             </a>
             <br></br>
          
-            <a href="/movies2" target="_blank"> 
+            <a href="/receita5/movies2" target="_blank"> 
                Movies 2
             </a>
             <br></br>
             
          
-            <a href="/movies3" target="_blank"> 
+            <a href="/receita5/movies3" target="_blank"> 
                Movies 3
             </a>
             <br></br>
          
-            <a href="/moviesR15" target="_blank"> 
+            <a href="/receita5/moviesR15" target="_blank"> 
                Movies Receita 15
             </a>
             <br></br>
